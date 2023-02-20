b125cae01b3c5c661f47f0ce6296a103a7ee6c8a
Hamid Sarfaraz <hamidsarfraz8@gmail.com>
2023-02-20 18:20:12 +0530

    added header.js

---
+++ course-bootcamp-app/src/Components/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function(){
+    return(
+        <div id="header-container">
+            
+            <div id="header-title">
+                <h2>Hello From header</h2>
+            </div>
+            
+            <div id="header-search">
+
+            </div>
+            <hr />
+        </div>
+        
+    )
+}
