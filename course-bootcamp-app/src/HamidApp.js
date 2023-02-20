c86572559c4fe71d00fb0fe15be57293dd0647fe
Hamid Sarfaraz <hamidsarfraz8@gmail.com>
2023-02-20 18:21:15 +0530

    updated HamidApp with header.js component and calls

--- course-bootcamp-app/src/HamidApp.js
+++ course-bootcamp-app/src/HamidApp.js
@@ -5,15 +5,15 @@ import Nav from './Components/Navbar';
 import Main from './Components/Main';
 import Contact  from './Components/Contact';
 import Map_Functionality from './Components/Map_Functionality';
-import b1 from './Images/pexels-daniel-217872.jpg';
-import b2 from './Images/pexels-fox-750841.jpg';
-import b3 from './Images/pexels-nishant-aneja-2393821.jpg';
-import b4 from './Images/pexels-pixabay-104842.jpg';
-import b5 from './Images/pexels-vikram-sundaramoorthy-1119796.jpg';
-import b6 from './Images/pexels-fox-750841.jpg';
+// import b1 from './Images/pexels-daniel-217872.jpg';
+// import b2 from './Images/pexels-fox-750841.jpg';
+// import b3 from './Images/pexels-nishant-aneja-2393821.jpg';
+// import b4 from './Images/pexels-pixabay-104842.jpg';
+// import b5 from './Images/pexels-vikram-sundaramoorthy-1119796.jpg';
+// import b6 from './Images/pexels-fox-750841.jpg';
 
 import moviesData from './Data/MoviesData.json';
-
+import Header from './Components/Header';
 
 function HamidApp() {
   // const img1 = "https://image.tmdb.org/t/p/w500/1NqwE6LP9IEdOZ57NCT51ftHtWT.jpg";
@@ -32,13 +32,33 @@ function HamidApp() {
       <Main /> */}
 
       {/* <Map_Functionality /> */}
-
+      <Header />
       <Contact name="John Doe" position="Architect" YOE={15} image={movie_posters[0]}/>
-      {/* <Contact name="Hamid Sarfaraz" position="TDP" YOE={1} image={b2}/>
-      <Contact name="Mayank Dwivedi" position="Manager" YOE={13} image={b3}/>
-      <Contact name="Gupta Milan" position="SDE" YOE={10} image={b4}/>
-      <Contact name="Aparna Kolanu"  position="Team Leader" YOE={10} image={b5}/>
-      <Contact name="New Comer"  position="" YOE={0} image={b6}/>  */}
+      <Contact name="Hamid Sarfaraz" position="TDP" YOE={1} image={movie_posters[1]}/>
+      <Contact name="Mayank Dwivedi" position="Manager" YOE={13} image={movie_posters[2]}/>
+      <Contact name="Gupta Milan" position="SDE" YOE={10} image={movie_posters[3]}/>
+      <Contact name="Aparna Kolanu"  position="Team Leader" YOE={10} image={movie_posters[4]}/>
+      <Contact name="New Comer"  position="" YOE={0} image={movie_posters[5]}/>
+      <Contact name="New Comer"  position="" YOE={0} image={movie_posters[6]}/>
+      <Contact name="New Comer"  position="" YOE={0} image={movie_posters[7]}/>
+      <Contact name="New Comer"  position="" YOE={0} image={movie_posters[8]}/>
+      <Contact name="New Comer"  position="" YOE={0} image={movie_posters[9]}/>
+
+      
+      
+
+      {/* <Contact image={movie_posters[0]}/>
+      <Contact image={movie_posters[1]}/>
+      <Contact image={movie_posters[2]}/>
+      <Contact image={movie_posters[3]}/>
+      <Contact image={movie_posters[4]}/>
+      <Contact image={movie_posters[5]}/>
+      <Contact image={movie_posters[6]}/>
+      <Contact image={movie_posters[7]}/>
+      <Contact image={movie_posters[8]}/>
+      <Contact image={movie_posters[9]}/> */}
+
+      {/* <Contact name="New Comer"  position="" YOE={0} image={movie_posters[5]}/>  */}
     </div>
   );
 }
